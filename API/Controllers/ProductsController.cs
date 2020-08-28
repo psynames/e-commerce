d9d6172386f869900577c3c271a1d2bfa2a992eb
psyname <48337480+psynames@users.noreply.github.com>
2020-08-28 23:42:23 +0200

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+
+        private readonly StoreContext _dbcontext;
+        public ProductsController(StoreContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _dbcontext.Products.FindAsync(id);
+            return Ok(product);
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _dbcontext.Products.ToListAsync();
+            return Ok(products);
+        }
+
+
+    }
+}
---
+++ API/Entities/Product.cs
@@ -0,0 +1,11 @@
+namespace API.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+
+
+    }
+}
